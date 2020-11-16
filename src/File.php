31d4c8b89cde5f7417bf50646258723612bd3cea
Bagus A Priambada <iam.adty@gmail.com>
2020-11-16 19:51:40 +0700

    rename recursive read function

--- src/File.php
+++ src/File.php
@@ -1,14 +1,14 @@
 <?php namespace PWC\Util;
 
 class File {
-    public static function recursive_read($src, $callable = null)
+    public static function recursiveRead($src, $callable = null)
     {
         $dir = opendir($src);
 
         while(($file = readdir($dir))) {
             if (($file != '.') && ($file != '..')) {
                 if (is_dir($src . '/' . $file)) {
-                    self::recursive_read($src . '/'. $file, $callable);
+                    self::recursiveRead($src . '/'. $file, $callable);
                 } else {
                     if (!is_null($callable)) {
                         $callable($src . '/' . $file);
