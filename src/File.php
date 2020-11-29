6fa485a6bd23af491f285feb23a52df6c6dea1c2
Bagus A Priambada <iam.adty@gmail.com>
2020-11-29 21:57:50 +0700

    recursive read check if dir exists

--- src/File.php
+++ src/File.php
@@ -4,20 +4,20 @@ class File
 {
     public static function recursiveRead($src, $callable = null)
     {
-        $dir = opendir($src);
-
-        while(($file = readdir($dir))) {
-            if (($file != '.') && ($file != '..')) {
-                if (is_dir($src . '/' . $file)) {
-                    self::recursiveRead($src . '/'. $file, $callable);
-                } else {
-                    if (!is_null($callable)) {
-                        $callable($src . '/' . $file);
+        if (file_exists($src)) {
+            $dir = opendir($src);
+            while(($file = readdir($dir))) {
+                if (($file != '.') && ($file != '..')) {
+                    if (is_dir($src . '/' . $file)) {
+                        self::recursiveRead($src . '/'. $file, $callable);
+                    } else {
+                        if (!is_null($callable)) {
+                            $callable($src . '/' . $file);
+                        }
                     }
                 }
             }
+            closedir($dir);
         }
-
-        closedir($dir);
     }
 }
